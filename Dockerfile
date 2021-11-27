c6b359206ff41aa957e5eccc6ba2f8d6a5b22e39
Cu64 <dongduongdev@gmail.com>
2021-11-27 17:39:50 +0700

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM kalilinux/kali-rolling:latest
+
+RUN apt-get -y update
+RUN apt-get -y upgrade
+RUN apt-get -y install kali-linux-core
+RUN apt-get -y install kali-tools-top10
+RUN apt-get -y install kali-desktop-xfce
+RUN apt-get -y install tightvncserver dbus dbus-x11 novnc net-tools
+
+
+ENV USER root
+ENV VNCEXPOSE 0
+ENV VNCPORT 5900
+ENV VNCPWD HaccZeHub
+ENV VNCDISPLAY 1920x1080
+ENV VNCDEPTH 16
+ENV NOVNCPORT 8080
+
+COPY entry.sh /entry.sh
+RUN chmod +x /entry.sh
+
+ENTRYPOINT [ "/entrypoint.sh" ]
---
+++ entry.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+mkdir -p /root/.vnc/
+echo $VNCPWD | vncpasswd -f > /root/.vnc/passwd
+chmod 600 /root/.vnc/passwd
+
+vncserver :0 -rfbport $VNCPORT -geometry $VNCDISPLAY -depth $VNCDEPTH -localhost > /var/log/vncserver.log 2>&1
+
+if [ ! -f /etc/ssl/certs/novnc_cert.pem -o ! -f /etc/ssl/private/novnc_key.pem ]
+then
+    openssl req -new -x509 -days 365 -nodes -subj "/C=US/ST=IL/L=Springfield/O=OpenSource/CN=localhost" -out /etc/ssl/certs/novnc_cert.pem -keyout /etc/ssl/private/novnc_key.pem > /dev/null 2>&1
+fi
+
+cat /etc/ssl/certs/novnc_cert.pem /etc/ssl/private/novnc_key.pem > /etc/ssl/private/novnc_combined.pem
+chmod 600 /etc/ssl/private/novnc_combined.pem
+
+/usr/share/novnc/utils/launch.sh --listen $NOVNCPORT --vnc localhost:$VNCPORT --cert /etc/ssl/private/novnc_combined.pem --ssl-only > /var/log/novnc.log 2>&1 &
+
+echo "Launch your web browser and open https://localhost:9020/vnc.html"
+echo "Verify the certificate fingerprint:"
+openssl x509 -in /etc/ssl/certs/novnc_cert.pem -noout -fingerprint -sha256
+
+tail -f /dev/null
